64b1b2c7ff3357b531a89b5ee1c86fc11cc4e702
vyom-chokshi <vyomchokshi4@gmail.com>
2026-08-23 17:29:26 +0530

    Time: 2 ms (16.94%), Space: 12.5 MB (27.1%) - LeetHub

---
+++ 0118-pascals-triangle/0118-pascals-triangle.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def generate(self, numRows):
+        """
+        :type numRows: int
+        :rtype: List[List[int]]
+        """
+        ans = []
+
+        for i in range(numRows):
+            row = [1] * (i + 1)
+
+            for j in range(1, i):
+                row[j] = ans[i - 1][j - 1] + ans[i - 1][j]
+
+            ans.append(row)
+
+        return ans
