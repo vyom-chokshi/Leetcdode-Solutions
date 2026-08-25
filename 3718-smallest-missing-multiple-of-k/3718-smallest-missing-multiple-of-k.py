7721bf176b675a98ba2ced09d66936348f696fbb
vyom-chokshi <vyomchokshi4@gmail.com>
2026-08-25 21:40:44 +0530

    Time: 3 ms (28.74%), Space: 12.3 MB (50.57%) - LeetHub

---
+++ 3718-smallest-missing-multiple-of-k/3718-smallest-missing-multiple-of-k.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def missingMultiple(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: int
+        """
+        x=k
+        while x in nums:
+            x+=k
+            
+        return x
+
+
+
+
+
+
+
+
+
