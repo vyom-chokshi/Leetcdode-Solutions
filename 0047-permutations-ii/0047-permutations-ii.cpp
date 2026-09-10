022f5c691cead00252fd617f6d9255baafb2a3db
vyom-chokshi <vyomchokshi4@gmail.com>
2026-09-10 22:34:18 +0530

    Time: 4 ms (45.94%), Space: 13.8 MB (25.17%) - LeetHub

--- 0047-permutations-ii/0047-permutations-ii.cpp
+++ 0047-permutations-ii/0047-permutations-ii.cpp
@@ -1,24 +1,25 @@
 class Solution {
 public:
-void solve(vector<int>&n,int index,set<vector<int>>&ans)
+void solve(vector<int>&n,int index,vector<vector<int>>&ans)
 {
     if(index==n.size())
     {
-        ans.insert(n);
+        ans.push_back(n);
     }
-
+    set<int> used;
     for(int i= index;i<n.size();i++)
     {
+        if(used.count(n[i]))
+            continue;
+        used.insert(n[i]);
         swap(n[index],n[i]);
         solve(n,index+1,ans);
         swap(n[index],n[i]);
     }
 }
     vector<vector<int>> permuteUnique(vector<int>& nums) {
-        set<vector<int>>ans;
-        solve(nums,0,ans);
-        vector<vector<int>> result(ans.begin(), ans.end());
-
-        return result;
+     vector<vector<int>>ans;
+     solve(nums,0,ans);
+     return ans;   
     }
 };
