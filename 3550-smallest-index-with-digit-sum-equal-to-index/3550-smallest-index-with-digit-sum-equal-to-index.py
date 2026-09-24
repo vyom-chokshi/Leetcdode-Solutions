d66d5ebd0ebcaada7828141761a3a1a45797fd05
vyom-chokshi <vyomchokshi4@gmail.com>
2026-09-24 21:00:25 +0530

    Time: 3 ms (58.41%), Space: 19.3 MB (29.46%) - LeetHub

---
+++ 3550-smallest-index-with-digit-sum-equal-to-index/3550-smallest-index-with-digit-sum-equal-to-index.py
@@ -0,0 +1,9 @@
+class Solution:
+    def smallestIndex(self, nums: List[int]) -> int:
+        for i, num in enumerate(nums):
+            digit_sum = sum(int(d) for d in str(num))
+            
+            if digit_sum == i:
+                return i
+        
+        return -1
